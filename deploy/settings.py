5cec02afe89e64e33b3b31332c964c2f2ff61bff
Andrew Karam <36473436+AndyDodss@users.noreply.github.com>
2019-03-25 21:38:10 +0200

    'ff'

--- deploy/settings.py
+++ deploy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'iy4pk93r=65)d#a!j1&417-xmr36**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['oursurv.herokuapp.com','surv1.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['oursurv.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -76,18 +76,12 @@ WSGI_APPLICATION = 'deploy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'task',
-        'USER': 'task_user',
-        'PASSWORD': 'your_password',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 # Password validation
@@ -127,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import dj_database_url
