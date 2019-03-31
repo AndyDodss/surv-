b3a0c0b752d6b0b5e0f93b331ba38cfcadbfd866
Andrew Karam <36473436+AndyDodss@users.noreply.github.com>
2019-03-31 15:28:58 +0200

    'login'

--- deploy/urls.py
+++ deploy/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('surv.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
+
     #singelton : only this app exist
     #path('', RedirectView.as_view(url='/surv/', permanent=True)),
 ] # static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
